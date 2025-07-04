0c56bd72bc197b5d5fd7c0fc4a5e6b30d019fe7f
Aryan Kuril <aryan@bombayblokes.com>
2025-07-04 12:02:03 +0530

    project added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,16 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // You can override specific rules here
+  
   {
-    files: ["**/*.ts", "**/*.tsx"], // Apply this override to TypeScript files
+    files: ["**/*.ts", "**/*.tsx"],
     rules: {
       "@next/next/no-img-element": "off",
     },
   },
 ];
-
-export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["s3-us-west-2.amazonaws.com"],
   },
-extends: ["next", "next/core-web-vitals", "eslint:recommended"],
-  rules: {
-    "@next/next/no-img-element": "off",
-  },
 };
 
 export default nextConfig;
